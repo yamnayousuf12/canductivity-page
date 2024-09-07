0e5f620ad988486b1f3ee7ce04f12d5e3c044074
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2024-09-07 21:04:38 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import ButtonSection from './components/ButtonSection';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <HomePage />
+    <ButtonSection />
+    </div>
+  );
+}
+
+export default App; 
+
+
+// App.js
+
+// import React from 'react';
+// import CarouselComponent from './components/CarouselComponent';
+// import ButtonSection from './components/ButtonSection';
+
+// function App() {
+//   return (
+//     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+//       <header className="mb-8">
+//         <img src="image/log.png" alt="Logo" className="mx-auto my-4" />
+//       </header>
+//       <CarouselComponent />
+//       <ButtonSection />
+//     </div>
+//   );
+// }
+
+// export default App;
+
